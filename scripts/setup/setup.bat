93b50b6a0b42dc34f751b94010bca9d5ca6e390e
Zach Metcalf <zachmetcalf@gmail.com>
2026-02-02 17:01:40 -0800

    new standards

--- scripts/setup/setup.bat
+++ scripts/setup/setup.bat
@@ -1,40 +1,31 @@
 :: Copyright (c) 2026 Zach Metcalf. All Rights Reserved.
 
 @echo off
-title setup
+title standards
 
 set cwd=%~dp0
 set projectdir=%cwd%..\..
-set shareddir=%projectdir%\..\shared
+set tempdir=%projectdir%\.standards
 
-pushd %projectdir%\..
-
-call :SetupGitRepo shared main https://github.com/zachmetcalf/shared
-
-popd
-
-pushd %shareddir%\scripts\setup
-
-call setup.bat
-
-popd
-
-echo setup completed
-exit /b 0
-
-:SetupGitRepo
-setlocal
-set repo=%~1
-set branch=%~2
-set url=%~3
-
-if not exist %repo% (
-	git clone --branch %branch% %url% %repo%
-) else (
-	pushd %repo%
-	git pull
-	popd
+if exist "%tempdir%" (
+	rmdir /s /q "%tempdir%"
 )
 
-endlocal
+mkdir "%tempdir%"
+
+pushd "%tempdir%"
+
+git clone --branch main https://github.com/zachmetcalf/zachmetcalf .
+
+popd
+
+pushd "%tempdir%\scripts\standards"
+
+call .\standards.bat "%projectdir%"
+
+popd
+
+rmdir /s /q "%tempdir%"
+
+echo standards completed
 exit /b 0
